ed61d05b6419907ad62450539b05e91c9438ef30
Hank Besser <hbess1113@gmail.com>
2017-01-02 16:48:06 -0600

    don't add Diffusion Class yet

--- brownian_manifold/__init__.py
+++ brownian_manifold/__init__.py
@@ -1,4 +1,4 @@
 from .manifold import Manifold
-from .diffusion import Diffusion
+#from .diffusion import Diffusion
 from .utils import vector_cross, arctan2, surface_sphere, surface_cylinder
 __version__ = '0.1.dev'
